dc5c92224a1557c5c138d73178a4cc97155552d0
srcsdk <srcsdk@users.noreply.github.com>
2021-02-03 19:25:00 -0500

    across add nolo exit condition with 30pct threshold

--- across.py
+++ across.py
@@ -50,6 +50,46 @@ def scan(ticker, period="1y"):
     return signals
 
 
+def scan_nolo(ticker, period="1y"):
+    """nolo: across but within 30% of 52wk low"""
+    rows = fetch_ohlc(ticker, period)
+    if not rows or len(rows) < 60:
+        return []
+
+    closes = [r["close"] for r in rows]
+    opens = [r["open"] for r in rows]
+
+    rsi_vals = rsi(closes, 14)
+    macd_line, signal_line, _ = macd(closes)
+    low_52 = fifty_two_week_low(closes)
+    gaps = gap_percent(opens, closes)
+
+    signals = []
+    for i in range(1, len(rows)):
+        if rsi_vals[i] is None or macd_line[i] is None:
+            continue
+        if signal_line[i] is None or macd_line[i - 1] is None:
+            continue
+        if signal_line[i - 1] is None:
+            continue
+
+        within_30 = closes[i] <= low_52[i] * 1.30
+        rsi_oversold = rsi_vals[i] < 30
+        macd_cross = (macd_line[i] > signal_line[i]
+                      and macd_line[i - 1] <= signal_line[i - 1])
+
+        if within_30 and rsi_oversold and macd_cross:
+            pct_above_low = (closes[i] - low_52[i]) / low_52[i] * 100
+            signals.append({
+                "date": rows[i]["date"],
+                "price": closes[i],
+                "rsi": rsi_vals[i],
+                "pct_above_52low": round(pct_above_low, 1),
+            })
+
+    return signals
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("usage: python across.py <ticker> [period]")
