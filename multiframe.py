6eebec1ebd710623d7ab9d8d9810dfe02619266b
srcsdk <srcsdk@users.noreply.github.com>
2021-04-13 20:19:00 -0600

    add weekly and monthly alignment to multiframe

--- multiframe.py
+++ multiframe.py
@@ -114,6 +114,46 @@ def timeframe_bias(rows, label=""):
     }
 
 
+def weekly_monthly_alignment(daily_rows):
+    """resample daily data to weekly/monthly and check trend alignment.
+
+    returns dict with trend direction per timeframe and whether
+    all timeframes agree on direction.
+    """
+    if not daily_rows or len(daily_rows) < 30:
+        return {"aligned": False, "direction": "neutral"}
+
+    weekly = aggregate_weekly(daily_rows)
+    monthly = aggregate_monthly(daily_rows)
+
+    def trend_direction(rows):
+        if len(rows) < 2:
+            return "neutral"
+        closes = [r["close"] for r in rows]
+        sma_vals = sma(closes, min(10, len(closes)))
+        last_sma = sma_vals[-1]
+        if last_sma is None:
+            return "neutral"
+        if closes[-1] > last_sma:
+            return "bullish"
+        elif closes[-1] < last_sma:
+            return "bearish"
+        return "neutral"
+
+    d_trend = trend_direction(daily_rows)
+    w_trend = trend_direction(weekly)
+    m_trend = trend_direction(monthly)
+
+    aligned = (d_trend == w_trend == m_trend and d_trend != "neutral")
+    return {
+        "daily": d_trend,
+        "weekly": w_trend,
+        "monthly": m_trend,
+        "aligned": aligned,
+        "direction": d_trend if aligned else "mixed",
+    }
+
+
 def multi_timeframe_analysis(ticker, period="2y"):
     """analyze a ticker across daily, weekly, and monthly timeframes.
 
