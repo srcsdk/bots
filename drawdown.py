21aed332251f38c0fd34ccf429229f409797786f
srcsdk <srcsdk@users.noreply.github.com>
2022-11-19 21:15:00 -0600

    improve gap analysis with classification by type

--- drawdown.py
+++ drawdown.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# improved: tracks recovery time from drawdowns
 """drawdown analysis and tracking"""
 
 
--- validate_ohlc.py
+++ validate_ohlc.py
@@ -1,121 +1,127 @@
 #!/usr/bin/env python3
-"""validate ohlc data from yahoo finance for common issues"""
+"""validate ohlc data quality and detect anomalies"""
 
 
-def validate_row(row):
-    """check a single ohlc row for data quality issues.
+def validate_bar(bar):
+    """check single ohlc bar for data integrity.
 
-    returns list of issue strings, empty if clean.
+    returns list of issues found (empty if valid).
     """
     issues = []
-
-    required = ["date", "open", "high", "low", "close"]
+    required = ("open", "high", "low", "close")
     for field in required:
-        if field not in row or row[field] is None:
+        if field not in bar:
             issues.append(f"missing {field}")
+            return issues
+        val = bar[field]
+        if not isinstance(val, (int, float)):
+            issues.append(f"{field} not numeric: {type(val)}")
+        elif val <= 0:
+            issues.append(f"{field} non-positive: {val}")
 
     if not issues:
-        o, h, l, c = row["open"], row["high"], row["low"], row["close"]
+        if bar["high"] < bar["low"]:
+            issues.append(f"high ({bar['high']}) < low ({bar['low']})")
+        if bar["open"] > bar["high"] or bar["open"] < bar["low"]:
+            issues.append("open outside high-low range")
+        if bar["close"] > bar["high"] or bar["close"] < bar["low"]:
+            issues.append("close outside high-low range")
 
-        if h < l:
-            issues.append(f"high ({h}) < low ({l})")
-        if o < l or o > h:
-            issues.append(f"open ({o}) outside high-low range")
-        if c < l or c > h:
-            issues.append(f"close ({c}) outside high-low range")
-
-        for field in ["open", "high", "low", "close"]:
-            if row[field] <= 0:
-                issues.append(f"{field} is non-positive: {row[field]}")
-
-    volume = row.get("volume", 0)
-    if volume is not None and volume < 0:
+    volume = bar.get("volume", 0)
+    if isinstance(volume, (int, float)) and volume < 0:
         issues.append(f"negative volume: {volume}")
 
     return issues
 
 
-def validate_series(rows):
-    """validate a series of ohlc rows.
+def validate_series(bars, max_gap_pct=20.0):
+    """validate a series of ohlc bars.
 
-    checks for gaps, duplicates, and per-row issues.
-    returns dict with validation results.
+    checks for gaps, missing dates, and price anomalies.
+    returns dict with summary and per-bar issues.
     """
-    if not rows:
-        return {"valid": False, "error": "empty data"}
+    if not bars:
+        return {"valid": False, "error": "empty series"}
 
-    all_issues = []
-    dates_seen = set()
+    all_issues = {}
+    gaps = []
 
-    for i, row in enumerate(rows):
-        row_issues = validate_row(row)
-        if row_issues:
-            all_issues.append({"index": i, "date": row.get("date", "?"),
-                               "issues": row_issues})
+    for i, bar in enumerate(bars):
+        bar_issues = validate_bar(bar)
+        if bar_issues:
+            all_issues[i] = bar_issues
 
-        date = row.get("date", "")
-        if date in dates_seen:
-            all_issues.append({"index": i, "date": date,
-                               "issues": ["duplicate date"]})
-        dates_seen.add(date)
+        if i > 0:
+            prev_close = bars[i - 1]["close"]
+            if prev_close > 0:
+                gap_pct = abs(bar["open"] - prev_close) / prev_close * 100
+                if gap_pct > max_gap_pct:
+                    gaps.append({
+                        "index": i,
+                        "date": bar.get("date", ""),
+                        "gap_pct": round(gap_pct, 2),
+                        "prev_close": prev_close,
+                        "open": bar["open"],
+                    })
 
     return {
         "valid": len(all_issues) == 0,
-        "total_rows": len(rows),
+        "total_bars": len(bars),
+        "invalid_bars": len(all_issues),
+        "large_gaps": len(gaps),
         "issues": all_issues,
-        "date_range": f"{rows[0].get('date', '?')} to {rows[-1].get('date', '?')}",
+        "gaps": gaps,
     }
 
 
-def check_timestamps(rows):
-    """validate date ordering and flag gaps > 5 trading days.
-
-    returns list of gap warnings with start/end dates.
-    """
-    if not rows or len(rows) < 2:
-        return []
-
+def detect_gaps(bars, min_gap_pct=2.0):
+    """find all price gaps in a series."""
     gaps = []
-    for i in range(1, len(rows)):
-        prev_date = rows[i - 1].get("date", "")
-        curr_date = rows[i].get("date", "")
-        if curr_date <= prev_date:
+    for i in range(1, len(bars)):
+        prev_close = bars[i - 1]["close"]
+        if prev_close <= 0:
+            continue
+        gap = (bars[i]["open"] - prev_close) / prev_close * 100
+        if abs(gap) >= min_gap_pct:
             gaps.append({
                 "index": i,
-                "issue": "out_of_order",
-                "prev": prev_date,
-                "curr": curr_date,
+                "date": bars[i].get("date", ""),
+                "gap_pct": round(gap, 2),
+                "direction": "up" if gap > 0 else "down",
             })
-            continue
-        if len(prev_date) >= 10 and len(curr_date) >= 10:
-            py = int(prev_date[:4])
-            pm = int(prev_date[5:7])
-            pd = int(prev_date[8:10])
-            cy = int(curr_date[:4])
-            cm = int(curr_date[5:7])
-            cd = int(curr_date[8:10])
-            prev_days = py * 365 + pm * 30 + pd
-            curr_days = cy * 365 + cm * 30 + cd
-            day_gap = curr_days - prev_days
-            if day_gap > 7:
-                gaps.append({
-                    "index": i,
-                    "issue": "large_gap",
-                    "prev": prev_date,
-                    "curr": curr_date,
-                    "approx_days": day_gap,
-                })
     return gaps
 
 
-def print_validation(result):
-    """display validation results"""
-    status = "PASS" if result["valid"] else "FAIL"
-    print(f"validation: {status} ({result['total_rows']} rows)")
-    print(f"date range: {result['date_range']}")
+def fill_forward(bars):
+    """fill missing values using last known good data."""
+    if not bars:
+        return bars
+    cleaned = []
+    last_good = None
+    for bar in bars:
+        issues = validate_bar(bar)
+        if not issues:
+            last_good = bar
+            cleaned.append(dict(bar))
+        elif last_good:
+            filled = dict(last_good)
+            filled["date"] = bar.get("date", last_good.get("date", ""))
+            filled["volume"] = 0
+            cleaned.append(filled)
+    return cleaned
 
-    if result.get("issues"):
-        print(f"\nissues ({len(result['issues'])}):")
-        for issue in result["issues"][:20]:
-            print(f"  row {issue['index']} ({issue['date']}): "
-                  f"{', '.join(issue['issues'])}")
+
+if __name__ == "__main__":
+    bars = [
+        {"date": "2022-01-03", "open": 100, "high": 105,
+         "low": 98, "close": 103, "volume": 1000},
+        {"date": "2022-01-04", "open": 103, "high": 107,
+         "low": 101, "close": 106, "volume": 1200},
+        {"date": "2022-01-05", "open": 130, "high": 135,
+         "low": 128, "close": 132, "volume": 5000},
+    ]
+    result = validate_series(bars)
+    print(f"valid: {result['valid']}, gaps: {result['large_gaps']}")
+    gaps = detect_gaps(bars)
+    for g in gaps:
+        print(f"  {g['date']}: {g['direction']} gap {g['gap_pct']}%")
